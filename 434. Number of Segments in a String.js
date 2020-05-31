ba46eb4fab88c28557f7f41ca7eeb978ad2d1ff8
tlrmacl <catasy@naver.com>
2020-05-31 17:49:12 +0900

    Change directory

--- 434. Number of Segments in a String.js
+++
@@ -1,5 +0,0 @@
-var countSegments = function(s) {
-    s = s.replace(/\s+/g, ' ').trim();
-    let array = s.split(' ');
-    return (s == "") ? 0 : array.length;
-};
--- javascript/434. Number of Segments in a String.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int countSegments(String s) {
-        s = s.replaceAll("\\s+", " ").trim();
-        String[] result = s.split(" ");
-        return (s.length() == 0) ? 0 : result.length;
-    }
-}
